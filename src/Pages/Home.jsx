e6cd33c86e2eb3a8f307c38dbea005eaff71ecfc
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-04-23 20:14:09 +0500

    Update the home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  let navigate=useNavigate()
   const [input, setInput] =useState('')
+  function handlejoin(){
+    navigate(`/room/${input}`)
+  }
   return (
     <div id='home'>
-        <input type="text" placeholder="Enter room ID" onChange={(e)=>setInput(e.target.value)} />
-        <button>Join Room</button>
+        <input type="text" placeholder="Enter room ID" value={input } onChange={(e)=>setInput(e.target.value)} />
+        <button onClick={handlejoin}>Join Room</button>
     </div>
   )
 }
