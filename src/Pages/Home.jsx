884c6676e5c3d0f325234288a8484824e1f3c0fb
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-04-24 19:50:17 +0500

    Update the css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,15 +3,17 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  let navigate=useNavigate()
-  const [input, setInput] =useState('')
-  function handlejoin(){
+  let navigate = useNavigate()
+  const [input, setInput] = useState('')
+  function handlejoin() {
     navigate(`/room/${input}`)
   }
   return (
     <div id='home'>
-        <input type="text" placeholder="Enter room ID" value={input} onChange={(e)=>setInput(e.target.value)} />
-        <button onClick={handlejoin}>Join Room</button>
+      <h1 className='talktime-logo'>TalkTime</h1>
+      <p className="text-gray-600 mb-6">Enter a room ID to start video calling!</p>
+      <input type="text" placeholder="Enter room ID" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handlejoin}>Join Room</button>
     </div>
   )
 }
